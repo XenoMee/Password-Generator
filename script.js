dad55e2bf87f213a6a13ec91984a54a2c819f23b
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-12 18:21:09 +0200

    concatenate symbol characters to character codes arrays if symbols checkbox is checked

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function generateRandomPassword(length, includeUppercase, includeNumbers, includ
     let charCodes = LOWERCASE_CHAR_CODES;
     if(includeUppercase) charCodes =  charCodes.concat(UPPERCASE_CHAR_CODES);
     if(includeNumbers) charCodes =  charCodes.concat(NUMBER_CHAR_CODES);
-    // if(includeSymbols) charCodes =  charCodes.concat(SYMBOL_CHAR_CODES);
+    if(includeSymbols) charCodes =  charCodes.concat(SYMBOL_CHAR_CODES);
 }
 
 // Create function to loop through ASCII codes

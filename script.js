9ea3f5775b14658d34d48223c312b7547e74e350
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-12 18:31:08 +0200

    assign the generated random password to the generated password input value

--- script.js
+++ script.js
@@ -20,13 +20,14 @@ const generatePasswordButton = document.getElementById("generate-button");
 const includeUppercaseEl = document.getElementById("uppercase-check");
 const includeNumbersEl = document.getElementById("numbers-check");
 const includeSymbolsEl = document.getElementById("symbols-check");
-const generatePassword = document.getElementById("generate-password");
+const generatedPassword = document.getElementById("generated-password");
 generatePasswordButton.addEventListener('click', function() {
     const length = charactersAmountNumber.value;
     const includeUppercase = includeUppercaseEl.checked;
     const includeNumbers = includeNumbersEl.checked;
     const includeSymbols = includeSymbolsEl.checked;
-    const generatedPassword = generateRandomPassword(length, includeUppercase, includeNumbers, includeSymbols);
+    const randomPassword = generateRandomPassword(length, includeUppercase, includeNumbers, includeSymbols);
+    generatedPassword.value = randomPassword;
 
 });
 

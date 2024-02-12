1ae77c1c5f301097b781a8f937e2cd0b90323a2e
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-12 17:50:06 +0200

    assign input's event target value with password length and range values

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ charactersAmountNumber.addEventListener('input', syncCharacterAmount);
 charactersAmountRange.addEventListener('input', syncCharacterAmount);
 
 function syncCharacterAmount(e) {
-    // const value = e.target.value;
-    // charactersAmountNumber.value = value;
-    // charactersAmountRange.value = value;
+    const value = e.target.value;
+    charactersAmountNumber.value = value;
+    charactersAmountRange.value = value;
 }

000faae7f84ae8fc0c10a794e6dc8f594fd82fd3
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-02-12 17:48:01 +0200

    store password length and range inputs inside a variable

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 // TODO: Add "copy on click"
 // TODO: Toggle symbols and numbers on/off
 // TODO: Light/Dark mode toggle button
+
+// TODO: Sync number and range inputs
+const charactersAmountNumber = document.getElementById("password-length");
+const charactersAmountRange = document.getElementById("password-range");
+
+// charactersAmountNumber.addEventListener('input', syncCharacterAmount);
+// charactersAmountRange.addEventListener('input', syncCharacterAmount);
+
+// function syncCharacterAmount(e) {
+//     const value = e.target.value;
+//     charactersAmountNumber.value = value;
+//     charactersAmountRange.value = value;
+// }
